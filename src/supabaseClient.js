897041d5204bd9670ff1ae133126d14d73d5ebd7
Pollyanna dos Santos Souza <pollyanna.souza@gpabr.com>
2026-03-24 16:42:31 -0300

    Segurança: variáveis de ambiente configuradas e chaves ocultas

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://wdvtuvohucyndqjnfpyh.supabase.co'
-const supabaseKey = 'sb_publishable_WUIsSwuV_kncGM-YfnT0EA_gnQlS_D3'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
